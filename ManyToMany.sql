1fad310f7a77d611f7bf61604ad722ea5359c769
Mohibur Rahman <mohiburrahman5509@gmail.com>
2022-03-09 15:56:43 +0600

    SQL file added.

---
+++ ManyToMany.sql
@@ -0,0 +1,18 @@
+#manytomany database
+create database manytomany;
+DROP DATABASE manytomany;
+
+# course table
+select * from course;
+drop table course;
+
+# drop a column
+alter table course drop column course_title;
+
+
+# student table
+select * from student;
+drop table student;
+
+# course_like table
+select * from course_like;
